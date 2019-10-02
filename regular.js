d7116191641c75689fb60be54b4373c37f28c87a
Declan <declan.l.johnson@gmail.com>
2019-10-02 10:01:16 -0400

    Start conversion menthods

--- regular.js
+++ regular.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 
-let fileName = "substring.json";
-let parsedFile = JSON.parse(fs.readFileSync(fileName, "utf8"));
-console.log(parsedFile);
-
-console.log(EvaluateNFA(parsedFile));
+// let fileName = "substring.json";
+// let parsedFile = JSON.parse(fs.readFileSync(fileName, "utf8"));
+// console.log(parsedFile);
+// 
+// console.log(EvaluateNFA(parsedFile));
 
 function EvaluateDFA(d) {
 	let state = d["initial_state"];
@@ -33,4 +33,42 @@ function EvaluateNFA(n) {
 		states = statebuff;
 	});
 	return n["accept_states"].some(r => states.includes(r));
+}
+
+let b = from_concat("11001");
+console.log(b);
+Object.keys(b["delta"]).forEach(key => {
+	console.log(key + " - " + b["delta"][key]);
+	console.log(b["delta"][key])
+});
+
+function from_concat(regexstr) {
+	let nfa = {
+		"initial_state": [0],
+		"states": regexstr.length + 1,
+		"delta": {
+			"0": {
+				"e": [1]
+			}
+		}
+	}
+	let staten = 1
+	regexstr.split("").forEach(character => {
+		let newState = {}
+		newState[character] = [staten+1]
+		nfa["delta"][staten] = newState;
+		staten += 1;
+	})
+	nfa["accept_states"] = [staten];
+	return nfa;
+}
+
+
+function RegexToNfa(regex) {
+	
+}
+
+module.exports = {
+	EvaluateDFA,
+	EvaluateNFA
 }
